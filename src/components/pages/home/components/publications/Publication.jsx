5c54f908a0cac4f95e592c21ebd25c0d075cb72c
Ntandoybalele Mngomezulu <87729665+NtandoMgo@users.noreply.github.com>
2024-06-13 12:12:16 +0000

    Styling, text over picture background

--- src/components/pages/home/components/publications/Publication.jsx
+++ src/components/pages/home/components/publications/Publication.jsx
@@ -1,9 +1,31 @@
-import './Publication.css'
-
-import React from 'react'
+import './Publication.css';
+import React from 'react';
+import arrow_btn from '../../../../../assets/half_arrow.png'
 
 export const Publication = () => {
   return (
-    <div>Publication</div>
-  )
-}
+    <div className="publication-container">
+      <div className="background-image image1">
+        <div className="overlay-text">
+          <p className='text-top'>CASE STUDY</p>
+          <p className='text-topic'>Strategy for a mining corporation</p>
+          <div className="view-btn">
+            <p>VIEW</p>
+              <img src={arrow_btn} alt="" className='no-color-filter'/>
+          </div>
+      </div>
+        <div className="overlay-text1">Text Over Image 1</div>
+      </div>
+      <div className="background-image image2">
+      <div className="overlay-text">
+          <p className='text-top'>PUBLICATION</p>
+          <p className='text-topic'>How Top100 Fortune Companies are using AI</p>
+          <div className="view-btn">
+            <p>VIEW</p>
+              <img src={arrow_btn} alt=""/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
