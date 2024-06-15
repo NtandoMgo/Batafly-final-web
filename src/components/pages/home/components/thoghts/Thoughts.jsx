75da55dc46d9cbbef5c95620aec9ff39fe52e83c
Ntandoybalele Mngomezulu <87729665+NtandoMgo@users.noreply.github.com>
2024-06-15 19:33:50 +0000

    thoughts complete

--- src/components/pages/home/components/thoghts/Thoughts.jsx
+++ src/components/pages/home/components/thoghts/Thoughts.jsx
@@ -1,8 +1,9 @@
-import './Thoughts.css'
-import chickens from '../../../../../assets/thoughts_home/chicken_photo.jpg'
-import vr from '../../../../../assets/thoughts_home/vr_photo.jpg'
+import './Thoughts.css';
+import chickens from '../../../../../assets/thoughts_home/chicken_photo.jpg';
+import vr from '../../../../../assets/thoughts_home/vr_photo.jpg';
+import arrow_btn from '../../../../../assets/half_arrow.png';
 
-import React from 'react'
+import React from 'react';
 
 const Thoughts = () => {
   return (
@@ -14,19 +15,29 @@ const Thoughts = () => {
           <img src={vr} alt="VR" />
           <div className="overlay-text">
             <p className="text1">Metaverse: How to prepare your company for a virtual life</p>
-            <p className="text2">Being in touch with the digital world is the first step in preparing for the future, as it is the new frontier for most organizations and clients.</p>
+            <p className="text2">Being in touch with the digital world is the first step in preparing 
+              for the future, as it is the new frontier for most organizations and clients.
+            </p>
+            <div className="explore-btn">
+                <p>EXPLORE</p>
+                <img src={arrow_btn} alt="arrow" className='white-arrow'/>
+            </div>
           </div>
         </div>
         <div className="image-right">
           <img src={chickens} alt="Chickens" />
+          <div className="additional-text">
+            <p className="text-below1">Metaverse: How to prepare your company for a virtual life</p>
+            <p className="text-below2">Being in touch with the digital world is the first step in preparing for the future, as it is the new frontier for most organizations and clients.</p>
+          </div>
+          <div className="explore-btn">
+                <p>EXPLORE</p>
+                <img src={arrow_btn} alt="arrow" className='white-arrow'/>
+            </div>
         </div>
       </div>
-      <div className="additional-text">
-        <p className="text-below1">Metaverse: How to prepare your company for a virtual life</p>
-        <p className="text-below2">Being in touch with the digital world is the first step in preparing for the future, as it is the new frontier for most organizations and clients.</p>
-      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Thoughts
+export default Thoughts;
