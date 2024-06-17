f0d95ba28c921c993d0b8217f50d402530eee22a
Ntandoybalele Mngomezulu <87729665+NtandoMgo@users.noreply.github.com>
2024-06-17 13:11:18 +0000

    About hero-conent

--- src/components/pages/about/About.jsx
+++ src/components/pages/about/About.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import './About.css'
+import blue_orange from '../../../assets/about_page/blu_orange.jpg'
 
 export const About = () => {
   return (
-    <div>Who We Are</div>
+    <div className="about-container">
+      <div className="hero-section">
+        <div className="hero-text">
+          <h1>Values</h1>
+          <p>Diversity</p>
+          <p>Transparency</p>
+          <p>Inclusivity</p>
+          <p>Integrity</p>
+          <p>Honesty</p>
+          <p>Making impact</p>
+        </div>
+        <div className="hero-image">
+          <img src={blue_orange} alt="Blue and Orange" />
+        </div>
+        <div className="hero-subtext">
+          <p>Additional text goes here. This could be a caption, quote, or any supplementary information.</p>
+        </div>
+      </div>
+      <div className="mission-section"></div>
+      <div className="commitment"></div>
+      <div className="motivation-section"></div>
+    </div>
   )
 }
